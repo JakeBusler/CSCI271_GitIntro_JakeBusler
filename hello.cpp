0557f5ab1acf8763eaf8ed5d1c8f0374145e78a4
Jake Busler <157557358+JakeBusler@users.noreply.github.com>
2026-05-18 23:29:52 -0700

    Add files via upload

---
+++ hello.cpp
@@ -0,0 +1,11 @@
+// CSCI 271 - GitHub Upload Practice
+// Name: Jake Busler
+// Section: 5132
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "Hello from GitHub! My first commit works." << endl;
+    return 0;
+}
